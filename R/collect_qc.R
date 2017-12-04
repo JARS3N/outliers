353173031949cdeb027a27189c2db54d3bbce4ac
JARS3N <JARS3N@users.noreply.github.com>
2017-12-04 09:28:18 -0500

    Update collect_qc.R

--- R/collect_qc.R
+++ R/collect_qc.R
@@ -1,6 +1,5 @@
 collect_qc<-function(dir=choose.dir()){
   require(dplyr)
-  list.files(dir,pattern="xls",full.names=T) %>%
-    lapply(.,munge_qc)%>%
+    lapply(list.files(dir,pattern="xls",full.names=T),munge_qc)%>%
     bind_rows()
 }
