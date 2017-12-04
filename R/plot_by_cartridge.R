306218687e266fa7c5a14565945c32cd2adb36dd
JARS3N <JARS3N@users.noreply.github.com>
2017-12-04 09:27:07 -0500

    Update plot_by_cartridge.R

--- R/plot_by_cartridge.R
+++ R/plot_by_cartridge.R
@@ -1,8 +1,10 @@
 plot_by_cartridge<-function(df,nameexp){
-  require(ggplot2);require(ggthemes);require(dplyr)
+  require(ggplot2);
+  require(ggthemes);
+  require(dplyr)
   nRuns<-(length(unique(df$fl)))
-  df %>% mutate(ctg=paste0(Lot,"_",sn)) %>%
-    ggplot(aes(grade)) +
+   data<- mutate(df,ctg=paste0(Lot,"_",sn)) 
+    ggplot(data,aes(grade)) +
     geom_bar(aes(fill=grade))+
     facet_wrap(~ctg)+
     ggtitle(paste0(nameexp,",n=",nRuns)) +
