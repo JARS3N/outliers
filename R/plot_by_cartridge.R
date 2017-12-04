c541411ae2aba9e36a835aa12b31e008232d0c69
JARS3N <JARS3N@users.noreply.github.com>
2017-12-04 11:38:20 -0500

    remove dply/pipes from plot_by_ctg

--- R/plot_by_cartridge.R
+++ R/plot_by_cartridge.R
@@ -1,10 +1,9 @@
 plot_by_cartridge<-function(df,nameexp){
   require(ggplot2);
   require(ggthemes);
-  require(dplyr)
   nRuns<-(length(unique(df$fl)))
-   data<- mutate(df,ctg=paste0(Lot,"_",sn)) 
-    ggplot(data,aes(grade)) +
+  df$ctg<-paste0(df$Lot,"_",data$sn)
+    ggplot(df,aes(grade)) +
     geom_bar(aes(fill=grade))+
     facet_wrap(~ctg)+
     ggtitle(paste0(nameexp,",n=",nRuns)) +
