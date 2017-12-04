611215b50f694033180befae0b7c450d865d29d9
JARS3N <JARS3N@users.noreply.github.com>
2017-12-04 11:35:46 -0500

    remove dplyr/pipes from plot_by_average

--- R/plot_by_averages.R
+++ R/plot_by_averages.R
@@ -1,15 +1,27 @@
 plot_by_averages <-function(df,nameexp){
   require(ggplot2);
   require(ggthemes);
-  require(dplyr)
   nWells<-length(unique(df$Well))
   nRuns<-(length(unique(df$fl)))
 
     
-    data<-summarize(
-      group_by(df,grade),
-              peraverage=round(n()/(nRuns* nWells)*100,3)
-    ) 
+ #   data<-summarize(
+ #     group_by(df,grade),
+ #             peraverage=round(n()/(nRuns* nWells)*100,3)
+ #   ) 
+  
+data<-do.call('rbind',
+  Map(function(x,y){
+    data.frame(grade=y,n=nrow(x))
+  },
+  x=split(df,df$grade),
+  y=names(split(df,A$grade))
+  )
+)
+data$peraverage<-round(n_table$n/(sum(n_table$n))*100,3)
+  
+  
+  
   
   
     ggplot(data,aes(grade,peraverage)) +
