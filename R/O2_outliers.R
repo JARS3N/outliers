6c8cbc86e747702a739b051ffb7543de0e8a4ab9
JARS3N <JARS3N@users.noreply.github.com>
2018-03-12 11:50:17 -0400

    Create O2_outliers.R

---
+++ R/O2_outliers.R
@@ -0,0 +1,17 @@
+O2_outliers<-function(X){
+require(dplyr)
+  O2 <-select(X$LVL,O2=mmHgO2,Well) %>%
+    mutate(.,mxO2=abs(O2-152))  %>%
+    group_by(.,Well) %>%
+    filter(.,mxO2==max(mxO2)) %>%
+    slice(.,1) %>% 
+    ungroup(.) %>%
+    mutate(.,grade=outliers::grade_ox(mxO2)) 
+ meta<-data.frame(
+    sn=X$sn,
+    Lot=X$Lot,
+    Instrument=X$Inst,
+    fl=X$file
+  )
+    merge(O2,meta)
+}
