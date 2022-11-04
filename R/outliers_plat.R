52ec2eaaf32fa40babc65382bed67819f0ae8f81
JARS3N <JARS3N@users.noreply.github.com>
2022-11-04 11:19:11 -0400

    Update outliers_plat.R

--- R/outliers_plat.R
+++ R/outliers_plat.R
@@ -1,11 +1,12 @@
 plat<-function (Instrument) 
 {
-    s1 <- as.character(substr(Instrument, 1, 1))
-    s2 <- as.character(substr(Instrument, 2, 2))
-    if (s1 == "2") {
-        return("XF96")
-    }
-    else {
-        return(c("XFe96", "XFe24", "XFp")[as.numeric(s2)])
-    }
+  Instrument<-as.numeric(Instrument)
+  s1 <- as.character(substr(Instrument, 1, 1))
+  s2 <- as.character(substr(Instrument, 2, 2))
+  if (s1 == "2") {
+    return("XF96")
+  }
+  else {
+    return(c("XFe96", "XFe24", "XFp","HS Mini","XFpro96")[as.numeric(s2)])
+  }
 }
