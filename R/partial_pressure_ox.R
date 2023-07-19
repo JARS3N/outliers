855794fa9ee686b043d19ce1cc0d406372cd0268
JARS3N <JARS3N@users.noreply.github.com>
2023-07-19 15:02:29 -0400

    Update partial_pressure_ox.R

--- R/partial_pressure_ox.R
+++ R/partial_pressure_ox.R
@@ -8,9 +8,11 @@
 #   DO(TC,vp(TC),atm)*(1/1000)*(1/32)*(18/1000)*HC(TC)*atm
 # })
 partial_pressure_ox <- Vectorize(function(TC, atm = 760) {
-  i <- (TC >= 30) + 1
-  coef <- c(0.678, 0.827)[i]
-  adj <- c(35, 49)[i]
-  0.005625 * (-0.0000058333 * TC ^ 3 + 0.0001821 * TC ^ 2 + 0.072405 * TC + 2.5443) * atm * 
-    (atm - (0.0456 * TC ^ 2 - 0.8559 * TC + 16.509)) * coef * (adj + TC)^-1
+ # i <- (TC >= 30) + 1
+ # coef <- c(0.678, 0.827)[i]
+ # adj <- c(35, 49)[i]
+ #0.005625 * (-0.0000058333 * TC ^ 3 + 0.0001821 * TC ^ 2 + 0.072405 * TC + 2.5443) * atm * 
+   # (atm - (0.0456 * TC ^ 2 - 0.8559 * TC + 16.509)) * coef * (adj + TC)^-1
+  message("reminder to replace partial_pressure_ox with pp::O2")
+  pp::O2(TC,atm)
 })
